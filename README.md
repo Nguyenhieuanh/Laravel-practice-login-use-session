566a4bdfcd3644952773a9ed51e2c1ec5b9d854c
Nguyenhieuanh <nguyenhieuanh.061626@gmail.com>
2020-06-01 14:20:43 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+#### [Thực hành] Lưu tên người dùng
+
+## Mục tiêu
+
+Sử dụng Session để lưu trữ thông tin người dùng khi đăng nhập và xóa bỏ thông tin khi người dùng đăng xuất.
+
+## Mô tả ứng dụng
+
+Trong phần này, chúng ta sẽ xây dựng một trang web với chức năng đăng nhập trong đó sẽ sử dụng Session để lưu trạng thái đăng nhập, các thông báo cần hiển thị ra ngoài view.
+
+Với chức năng đăng xuất chúng ta sẽ xóa Session lưu trữ trạng thái đăng nhập.
---
+++ app/Http/Controllers/BlogController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class BlogController extends Controller
+{
+    public function showBlog(Request $request)
+    {
+        // Kiểm tra Session login có tồn tại hay không
+        if ($request->session()->has('login') && $request->session()->get('login')) {
+
+            // Session login tồn tại và có giá trị là true, chuyển hướng người dùng đến trang blog
+            return view('blog');
+        }
+
+        // Session không tồn tại, người dùng chưa đăng nhập
+        // Gán một thông báo vào Session not-login
+        $message = 'Bạn chưa đăng nhập.';
+        $request->session()->flash('not-login', $message);
+
+        // Chuyển hướng về trang đăng nhập
+        return view('login');
+    }
+
+    public function logout(Request $request)
+    {
+        // Xóa Session login, đưa người dùng về trạng thái chưa đăng nhập
+        $request->session()->flush();
+        return view('login');
+    }
+}
